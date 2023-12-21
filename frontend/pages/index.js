29b5966f6a1268d28118ad3eb802401c3b8e941e
domkinger <dom.kinger@gmail.com>
2023-12-21 02:23:57 -0500

    added support for youtube shorts

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -43,7 +43,8 @@ const HomePage = () => {
   }, []);
 
   const extractVideoId = (url) => {
-    const regExp = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/;
+    // Updated regular expression to match both standard YouTube and Shorts URLs
+    const regExp = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=|shorts\/)([^#\&\?]*).*/;
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
   };
